9947756005c926a35459422b7ef4b7851f5efdea
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-09-12 06:40:12 -0700

    fix compilatio error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -7,7 +7,7 @@
  * Return: pointer to new right child node
  */
 
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_right_node = NULL;
 
