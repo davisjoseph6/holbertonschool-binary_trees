347236f20f79bccbcb46c8859c7cb4b148f5b560
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-09-12 07:12:01 -0700

    fixing betty error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/*
+/**
  * binary_tree_insert_right - inserts a node as the right-child of another node
  * @parent: pointer to parent
  * @value: value to store in the new node
