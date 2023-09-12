81807c1ea5cf92406cce23cad4c1ca4202af989d
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-09-12 05:49:19 -0700

    create a function that inserts a new left node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -20,6 +20,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	new_left_node->n = value;
 	new_left_node->parent = parent;
+	new_left_node->left = NULL;
 	new_left_node->right = NULL;
 	
 	if (parent->left != NULL)
