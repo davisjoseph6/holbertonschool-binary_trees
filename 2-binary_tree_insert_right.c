453b8c5d38994ed8e09831f6bac987a9c9855c8e
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-09-12 06:43:21 -0700

    fix compilation error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 	new_right_node->n = value;
 	new_right_node->parent = parent;
-	new_right_node->right = NULL;
+	new_right_node->left = NULL;
 	new_right_node->right = NULL;
 
 	if (parent->right != NULL)
