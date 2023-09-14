1f5bb788e8951b0204cb7e81390765f8009dcd56
“Aicha-ch” <chwikhi.aycha@gmail.com>
2023-09-14 01:57:35 -0700

    revise the seconf if condition

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -15,7 +15,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		return (0);
 	}
 
-	if (tree->left != NULL && tree->right != NULL)
+	if (tree->left != NULL || tree->right != NULL)
 	{
 	size_left = binary_tree_nodes(tree->left);
 	size_right = binary_tree_nodes(tree->right);
